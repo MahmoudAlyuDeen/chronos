04997bf66ccf951f4f27ec03ef11ac4986f8468c
mahmoud <mahmoudalyudeen@gmail.com>
2017-09-03 04:50:26 +0200

    -limited schedule to display 6 prayers -fetching prayers a month ahead -added location menu item to home screen -displayed prayer icons -day header init (today/tomorrow)

--- app/src/main/java/com/afterapps/chronos/home/PrayersAdapter.java
+++ app/src/main/java/com/afterapps/chronos/home/PrayersAdapter.java
@@ -1,6 +1,9 @@
 package com.afterapps.chronos.home;
 
 import android.content.Context;
+import android.graphics.Typeface;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +14,9 @@ import android.widget.TextView;
 import com.afterapps.chronos.Constants;
 import com.afterapps.chronos.R;
 import com.afterapps.chronos.beans.Prayer;
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.weather_icons_typeface_library.WeatherIcons;
 
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
@@ -78,9 +84,55 @@ class PrayersAdapter extends RecyclerView.Adapter<PrayersAdapter.PrayerViewHolde
             HashMap<String, String[]> prayerNames = Constants.PRAYER_NAMES;
             String prayerTitle = prayerNames.get(prayer.getWhichPrayer())[mArabic ? 2 : 0];
             String prayerSubtitle = prayerNames.get(prayer.getWhichPrayer())[mArabic ? 3 : 1];
+            IconicsDrawable prayerIcon = getPrayerIcon(prayer);
+            int textColor = getTextColor();
+            int textStyle = getTextStyle();
             mItemPrayerTitleTextView.setText(prayerTitle);
             mItemPrayerSubtitleTextView.setText(prayerSubtitle);
             mItemPrayerTimingTextView.setText(timeFormat.format(prayer.getTimestamp()));
+            mItemPrayerLogoImageView.setImageDrawable(prayerIcon);
+            mItemPrayerTitleTextView.setTypeface(null, textStyle);
+            mItemPrayerSubtitleTextView.setTypeface(null, textStyle);
+            mItemPrayerTitleTextView.setTextColor(textColor);
+            mItemPrayerSubtitleTextView.setTextColor(textColor);
+            mItemPrayerTimingTextView.setTextColor(textColor);
+        }
+
+        private int getTextColor() {
+            return ContextCompat.getColor(mContext,
+                    getAdapterPosition() == 0 ? R.color.colorAccent : R.color.colorTextPrimary);
+        }
+
+        private int getTextStyle() {
+            return getAdapterPosition() == 0 ?
+                    Typeface.BOLD : Typeface.NORMAL;
+        }
+
+        @NonNull
+        private IconicsDrawable getPrayerIcon(Prayer prayer) {
+            IconicsDrawable prayerIcon = new IconicsDrawable(mContext);
+            switch (prayer.getWhichPrayer()) {
+                case "fajr":
+                    prayerIcon.icon(WeatherIcons.Icon.wic_stars);
+                    break;
+                case "sunrise":
+                    prayerIcon.icon(WeatherIcons.Icon.wic_sunrise);
+                    break;
+                case "dhuhr":
+                    prayerIcon.icon(WeatherIcons.Icon.wic_day_sunny);
+                    break;
+                case "asr":
+                    prayerIcon.icon(WeatherIcons.Icon.wic_day_cloudy_high);
+                    break;
+                case "maghrib":
+                    prayerIcon.icon(WeatherIcons.Icon.wic_sunset);
+                    break;
+                case "isha":
+                    prayerIcon.icon(FontAwesome.Icon.faw_moon_o);
+                    break;
+            }
+            prayerIcon.color(ContextCompat.getColor(mContext, R.color.colorAccent));
+            return prayerIcon;
         }
     }
 }
